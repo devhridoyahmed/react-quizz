065e7f1f051649ef586e2a9bc96d181450ccb69f
Hridoy Ahmed <hridoyahmed47@gmail.com>
2025-02-20 07:04:51 +0600

    based on answer given or not next button will show up- feature is added in quiz app

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Questions";
+import NextQuestion from "./NextQuestion";
 
 const initialState = {
   questions: [],
@@ -45,6 +46,8 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error("unknown action");
   }
@@ -75,11 +78,14 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextQuestion dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
